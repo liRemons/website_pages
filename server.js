958a66517039ac3e02e09b35b8556c3eefbd6000
liRemons <remons@foxmail.com>
2022-03-17 23:42:31 +0800

    feat: server.js

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const path = require('path');
 const app = express();
-const port = 3000;
+const port = 80;
 app.use(express.static("dist", { maxAge: 1000 * 3600 }));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './dist/@website_pages/home/index.html'))
+})
 app.listen(port, () => console.log(`Example app listening on port port!`));
