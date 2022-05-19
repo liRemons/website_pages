3b1108dd287b24e36863bf38d0bc877a9a08ca0e
liRemons <remons@foxmail.com>
2022-05-20 00:04:00 +0800

    update style

--- src/components/CardList/index.jsx
+++ src/components/CardList/index.jsx
@@ -5,20 +5,18 @@ import { HOST } from '@utils';
 import style from './index.less';
 
 function CardList({ list, itemClick }) {
-  
+
   const renderImg = (url) => {
-    return url ? <img src={`${HOST}${url}`} alt="" /> :  <span className={style.icon}><FileTextTwoTone /></span>
+    return url ? <img src={`${HOST}${url}`} alt="" /> : <span className={style.icon}><FileTextTwoTone /></span>
   }
-  
+
   return <List
     grid={{ gutter: 24, column: 4 }}
     dataSource={list}
     renderItem={item => (
       <List.Item onClick={() => itemClick(item)} className={classNames('shadow', style.cardItem)}>
         <Card title={item.title}>
-          {
-            item.icon ? <span className={style.icon}> {item.icon} </span> : renderImg(item.url)
-          }
+          <span className={style.icon}> {item.icon || renderImg(item.url)} </span>
         </Card>
       </List.Item>
     )}
