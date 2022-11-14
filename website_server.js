62a8b1f07b11b3f6e29a23b2c5986962256d98d5
Remons <remons@foxmail.com>
2022-11-14 17:27:56 +0800

    Update website_server.js

--- website_server.js
+++ website_server.js
@@ -7,6 +7,10 @@ const port = 80;
 const https_port = 443;
 app.use(express.static("dist", { maxAge: 1000 * 3600 }));
 app.get('/', (req, res) => {
+  if (req.protocol === 'http') {
+    res.redirect(302, 'https://remons.cn');
+    res.end()
+  }
   res.sendFile(path.resolve(__dirname, './dist/@website_pages/home/index.html'))
 })
 const https_options = {
