c456d24da775d3135ac14e02603e97060da8d35b
Remons <remons@foxmail.com>
2022-11-14 14:44:42 +0800

    Update List.jsx

--- src/apps/transcoderQR/pages/List.jsx
+++ src/apps/transcoderQR/pages/List.jsx
@@ -1,19 +1,42 @@
 import React, { useState } from 'react';
-import { Form, Button, message } from 'antd';
+import { Form, Button, message, Modal } from 'antd';
 import FormItem from '@components/Form';
 import Fixed from '@components/Fixed';
 import Header from '@components/Header';
 import Container from '@components/Container';
+import ScanQr from '@components/ScanQr';
 import '@assets/css/index.global.less';
 import { copy } from 'methods-r';
 
+let obj = {};
 export default function List() {
   const [arr, setArr] = useState([]);
+  const [qrVisible, setQrVisible] = useState(false);
   const [form] = Form.useForm();
   const items = [
-    { label: '转码前', name: 'encode', component: 'textarea', componentProps: { rows: 4 } },
+    { label: '转码前', name: 'encode', component: 'textarea', componentProps: { rows: 4, allowClear: true } },
     { label: '转码后', name: 'decode', component: 'textarea', componentProps: { rows: 4 } }
   ];
+
+  const getQrValCallback = (val) => {
+    const [_, itemInfo, text] = val.split('__');
+    if (itemInfo) {
+      const [key, length] = itemInfo.split('_');
+      obj[key] = text;
+      if (Object.keys(obj).length === +length) {
+        let text = '';
+        for (const key in obj) {
+          text += obj[key]
+        }
+        form.setFieldValue('encode', text);
+        setTimeout(() => {
+          onSubmit();
+          setQrVisible(false);
+        }, 200);
+      }
+    }
+
+  }
 
   const onReset = () => {
     form.resetFields();
@@ -30,6 +53,10 @@ export default function List() {
     setArr(arr);
   };
 
+  const scanQr = () => {
+    setQrVisible(true)
+  }
+
   const onCopy = () => {
     copy(document.getElementById('decode').value)
     message.success('复制成功');
@@ -45,17 +72,23 @@ export default function List() {
           }
         </Form>
         <div className='tc'>
+          <Button type="primary" htmlType="submit" className='m-r-20' onClick={scanQr}>
+            扫描二维码
+          </Button>
           <Button type="primary" htmlType="submit" className='m-r-20' onClick={onSubmit}>
             PUSH
           </Button>
           <Button htmlType="button" onClick={onReset}>
             重置
           </Button>
-          { <Button type="primary" htmlType="button" className='m-l-20' onClick={onCopy}>
+          {<Button type="primary" htmlType="button" className='m-l-20' onClick={onCopy}>
             copy
           </Button>}
         </div>
         <Fixed />
+        <Modal bodyStyle={{ paddingTop: '64px' }} onOk={() => setQrVisible(false)} onCancel={() => setQrVisible(false)} open={qrVisible} destroyOnClose>
+          {qrVisible && <ScanQr getQrValCallback={getQrValCallback} />}
+        </Modal>
       </div>
     } />
     ;
