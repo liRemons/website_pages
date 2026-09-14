93b37c8a02208d67c2cfeff9e729aa72a59af552
liRemons <remons@foxmail.com>
2026-09-14 15:50:01 +0800

    feat: 更换vditor

--- config/cdn.js
+++ config/cdn.js
@@ -5,7 +5,7 @@ const js = [
   { externalsName: 'mobx-react', url: "https://registry.npmmirror.com/mobx-react/9.2.0/files/dist/mobxreact.umd.production.min.js" },
   { externalsName: 'mermaid', url: "https://registry.npmmirror.com/mermaid/11.16.0/files/dist/mermaid.min.js" },
   { externalsName: 'antd', url: "https://registry.npmmirror.com/dayjs/1.11.12/files/dayjs.min.js" },
-  { externalsName: 'antd', url: "https://registry.npmmirror.com/antd/5.20.0/files/dist/antd.min.js" },
+  { externalsName: 'antd', url: "https://registry.npmmirror.com/antd/5.29.3/files/dist/antd.min.js" },
   { externalsName: 'markdown-it', url: "https://registry.npmmirror.com/markdown-it/15.0.1/files/dist/browser/markdown-it.umd.min.js" },
   { externalsName: '@wangeditor/editor', url: "https://registry.npmmirror.com/@wangeditor/editor/5.1.23/files/dist/index.js" },
   { externalsName: 'highlight.js', url: "https://unpkg.com/@highlightjs/cdn-assets@11.12.0/highlight.min.js" },
--- src/apps/reMark/pages/List/index.jsx
+++ src/apps/reMark/pages/List/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Alert, Input } from 'antd';
-import { AlertOutlined } from '@ant-design/icons';
+import { Alert } from 'antd';
 import Fixed from '@components/Fixed';
 import Container from '@components/Container';
 import Header from '@components/Header';
 import handleContent from '../../handle.md';
 import '@assets/css/index.global.less';
-import { MarkdownEditor, useRegisterToolbar } from 'remons-markdown-editor'
+import { MarkdownEditor } from 'remons-markdown-editor'
 import { registerAll, excludedSelectors } from 'remons-markdown-plugins';
 import 'remons-markdown-editor/style.css'
 import 'remons-markdown-plugins/style.css'
@@ -14,16 +13,6 @@ import 'remons-markdown-plugins/style.css'
 import style from './index.module.less';
 
 function List() {
-  useRegisterToolbar({
-    name: 'badge',
-    label: '警告框',
-    icon: <AlertOutlined />,
-    fields: [{ key: 'type', defaultValue: 'price' }, { key: 'content', defaultValue: 'ce' }],
-    renderDialog: (props, onChange) => (
-      <Input value={props.level} onChange={e => onChange('content', e.target.value)} />
-    ),
-  })
-
   return <>
     <Container
       header={<Header name='markdown 编辑器' leftPath={`/${APP_NAME}/tool`} handleContent={handleContent} />}
